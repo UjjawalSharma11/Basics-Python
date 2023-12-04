98c7fea69a98c5a54b34c85db2c745c7323a5df8
UjjawalSharma11 <149385700+UjjawalSharma11@users.noreply.github.com>
2023-12-04 19:21:18 +0530

    Add files via upload

---
+++ 1). Basics.py
@@ -0,0 +1,85 @@
+# Basics Of Python
+print ("Twinkle Twinkle Little Star") # Print is a function.
+a=2
+b=3
+print(a*b) # Python can act as calculator.
+
+# Variables & Datatypes
+""" a="Ujjawal Sharma"
+b=11
+c=3.8
+# 2d=23 
+
+print(a) # String
+print(b) # Integers
+print(c) # Float
+
+# Type
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# Type Function & Typecasting :
+a = "11"
+print(type(a))
+# print(a+23)
+a = int(a)
+print(type(a))
+print(a+23)
+S = "a-z"
+L = list (S)
+print (L)
+
+Set1 = {1, 2.3, "Four"}
+D = dict (Set1)
+
+D = {1:"One", 2:"Two", 3:"Three"}
+L = list (D)
+
+
+# Order & Characterstics
+print (ord('U'))
+print (ord ('P')) 
+print (chr(85))
+print (chr(80))
+
+# Size
+import sys
+print ( sys.getsizeof("Ujjawal") )
+print ( sys.getsizeof(11) )
+
+# Input Function :
+a = input("Enter your name: ")
+print(a)
+
+b = int(input("Enter your DOB: "))
+print(b)
+print(type(b))
+"""
+"""
+# Q1).
+a = int(input("a: "))
+b = int(input("b: "))
+print(a+b) 
+
+
+# Q2).
+a = int(input("a: "))
+b = 2
+print("Remainder is", a/2)
+
+# Q3).
+a = input("Value is: ")
+print(a)
+print(type(a))
+
+# Q4).
+a = int(input("a: "))
+b = int(input("b: "))
+c = print(" Average of a & b is = ", (a+b)/2)
+
+# Q5).
+a = int(input("a: "))
+print("Square of a number is",a**2)
+"""
